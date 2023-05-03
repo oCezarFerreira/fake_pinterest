e1b850a9b4a98c6b13f80e01c5fd81921ff9bfcb
Cezar Ferreira <cezarleferr@gmail.com>
2023-05-02 23:41:39 -0400

    Criada a logica de Login

--- fake_pinterest/__init__.py
+++ fake_pinterest/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
-database = SQLAlchemy(app)
+app.config["SECRET_KEY"] = "375d9bee495014ecbbe60d43722fa5fb"
 
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'home_page'
 
 from fake_pinterest import routes
--- fake_pinterest/routes.py
+++ fake_pinterest/routes.py
@@ -1,12 +1,13 @@
 # criar as rotas do site
-
 from flask import render_template, url_for
 from fake_pinterest import app
+from flask_login import login_required
 
 @app.route('/')
 def home_page():
     return render_template('home_page.html')
 
 @app.route('/perfil/<usuario>')
+@login_required
 def perfil(usuario):
     return render_template('perfil.html', usuario=usuario)
