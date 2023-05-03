4040fae54788ffe04968878c689f29122495e65a
Cezar Ferreira <cezarleferr@gmail.com>
2023-05-03 17:42:38 -0400

    Implementado fomulario de login e formulario de criar conta

--- fake_pinterest/routes.py
+++ fake_pinterest/routes.py
@@ -2,10 +2,17 @@
 from flask import render_template, url_for
 from fake_pinterest import app
 from flask_login import login_required
+from fake_pinterest.forms import FormLogin, FormCriarConta
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home_page():
-    return render_template('home_page.html')
+    formLogin = FormLogin()
+    return render_template('home_page.html', form=formLogin)
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criar_conta():
+    formCriarConta = FormCriarConta()
+    return render_template("criar_conta.html", form=formCriarConta)
 
 @app.route('/perfil/<usuario>')
 @login_required
